2633bc5fe1ace65f57da918bbbf751e26074032f
Jeffrey Expectacion <expectacionjeffrey@gmail.com>
2026-08-04 02:12:32 +0800

    # feat: add in-memory job applications endpoint

---
+++ JobTracker.Api/Controllers/JobApplicationsController.cs
@@ -0,0 +1,54 @@
+﻿using JobTracker.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace JobTracker.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class JobApplicationsController : ControllerBase
+{
+    [HttpGet]
+    public List<JobApplication> Get()
+    {
+        string[] Companynames = new string[] { "Ubisoft", "Microsoft", "Google", "Amazon", "Facebook" };
+
+        int numberOfApplications = Companynames.Length;
+
+        List<JobApplication> ListOfApplications = new List<JobApplication>();
+
+        for (int i = 0; i < numberOfApplications; i++)
+        {
+            JobApplication application = new JobApplication
+            {
+
+                Id = 1 + i,
+                CompanyName = Companynames[i],
+                PositionTitle = ChosenPositionTitle(),
+                Status = NewStatus()
+            };
+
+            ListOfApplications.Add(application);
+        }
+
+        return ListOfApplications;
+    }
+
+    static string ChosenPositionTitle()
+    {
+        string[] PositionTitles = new string[] { "Unity Developer", "Backend Developer", "Frontend Developer", "Fullstack Developer", "Game Designer" };
+        int index = Random.Shared.Next(PositionTitles.Length);
+        string chosenPositionTitle = PositionTitles[index];
+
+        return chosenPositionTitle;
+    }
+
+    static string NewStatus()
+    {
+        string[] Statuses = new string[] { "applied", "interviewing", "offered", "rejected" };
+        int index = Random.Shared.Next(Statuses.Length);
+        string chosenStatus = Statuses[index];
+
+        return chosenStatus;
+    }
+}
---
+++ JobTracker.Api/Models/JobApplication.cs
@@ -0,0 +1,10 @@
+﻿namespace JobTracker.Api.Models
+{
+    public class JobApplication
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; } = string.Empty;
+        public string PositionTitle { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+}
