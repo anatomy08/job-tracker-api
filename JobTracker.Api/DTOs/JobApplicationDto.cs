3add2d77b4185c7c4c4bc2a0ac0b651ea8e80320
Jeffrey Expectacion <144685556+anatomy08@users.noreply.github.com>
2026-08-11 23:30:44 +0800

    Add JobApplication response DTO

---
+++ JobTracker.Api/DTOs/JobApplicationDto.cs
@@ -0,0 +1,7 @@
+namespace JobTracker.Api.DTOs;
+
+public class JobApplicationDto
+{
+    public string CompanyName { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+}
